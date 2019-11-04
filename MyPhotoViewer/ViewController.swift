7b051abf0bde271809074fdd68854f994ab234bc
Moon Yohan <juanagustin@naver.com>
2019-11-04 17:10:42 +0900

    Initial Commit

---
+++ MyPhotoViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPhotoViewer
+//
+//  Created by Webcash on 2019/11/04.
+//  Copyright © 2019 GMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
